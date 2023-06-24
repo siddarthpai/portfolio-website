ca1fdc5c37e8b6530cb36c32ec8f3f52d257dcd1
Siddarth Pai <siddarthdpai@gmail.com>
2023-06-24 21:37:49 +0530

    working on about us

--- portfolio/src/components/about/experience.js
+++ portfolio/src/components/about/experience.js
@@ -2,28 +2,53 @@ export default function Experience() {
   return (
     <div className="lg:ml-28mr-28 p-10">
       <h1 className="mt-32 text-3xl lg:text-6xl text-yellow-100 mb-10 ml-14">My Experience:</h1>
-      <table class="table-auto text-white text-center m-32">
-        <thead>
+      <table class="table-auto text-white text-center border border-white">
+        <thead className="text-center p-5">
           <tr>
             <th>Year</th>
             <th>Experience</th>
           </tr>
         </thead>
-        <tbody class="m-32">
+        <tbody>
           <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
+            <td>2023</td>
+            <td className="text-left">
+              ●{' '}
+              <a href="https://www.linkedin.com/posts/siddarth-pai-ba993a219_i-am-delighted-to-announce-that-my-team-has-activity-7058773162825547776-8D1k?utm_source=share&utm_medium=member_desktop">
+                Won First Place in the FOSS and Julia Track
+              </a>
+              🥇 at Ingenius PES University <br />
+              ●Built Websites for{' '}
+              <a href="https://www.instagram.com/peshackerspace.ecc/">HackerSpace PESUECC</a> using
+              Svelte+Tailwind CSS <br />
+              ●Became the Head of HR for the{' '}
+              <a href="https://www.instagram.com/p/Cn9QcYtJqBa/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==">
+                The Entrepreneurship Club of PESUECC
+              </a>{' '}
+              💵 <br />
+              ●Completed Level-1 in <a href="https://www.instagram.com/cie.pesu/">CIE</a> 📓
+              <br />
+              ●Started Exploring Rust and GO!💻 <br />
+              ●Built Websites for <a href="">Emtrepreneurship Club of PESUECC</a> using
+              Vite+Tailwind CSS <br />
+              ●Started Working on Research Paper realted to AI/ML 📝
+              <br />
+              ●Built a Website using Next.js+Tailwind CSS for a startup{' '}
+              <a href="https://nextwebsite-six.vercel.app/">OffTheShelf</a> <br />
+              ●Contributed to FOSS chapter of HackerSpace with a blog on{' '}
+              <a href="https://sharp-wright-d62960.netlify.app/posts/introduction-to-flutter/">
+                Flutter
+              </a>{' '}
+              <br />
+            </td>
           </tr>
           <tr>
-            <td>Witchy Woman</td>
-            <td>The Eagles</td>
-            <td>1972</td>
+            <td>2022</td>
+            <td className="text-left">The Eagles</td>
           </tr>
           <tr>
-            <td>Shining Star</td>
-            <td>Earth, Wind, and Fire</td>
-            <td>1975</td>
+            <td>2021</td>
+            <td className="text-left">Earth, Wind, and Fire</td>
           </tr>
         </tbody>
       </table>
